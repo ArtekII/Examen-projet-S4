7364d4702e2fabbca5e4d0cb75cbeb95d86d9a43
ArtekII <216646542+ArtekII@users.noreply.github.com>
2026-07-20 08:54:17 +0300

    init-base

---
+++ base.sql
@@ -0,0 +1,38 @@
+---- INIT TABLE v1 ----
+CREATE TABLE operateurs(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_operateur VARCHAR(255) NOT NULL,
+    prefixe VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE type_operations(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE clients(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_client VARCHAR(255) NOT NULL,
+    numero VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE configurations_transaction(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    borne_min DECIMAL(10, 2) DEFAULT 0.00,
+    borne_max DECIMAL(10, 2) DEFAULT 0.00,
+    montant_frais DECIMAL(10, 2) DEFAULT 0.00
+);
+
+CREATE TABLE operation_mouvement(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_emetteur INTEGER NOT NULL,
+    id_beneficiaire INTEGER NOT NULL,
+    id_operateur INTEGER NOT NULL,
+    id_type_operation INTEGER NOT NULL,
+    montant DECIMAL(10, 2) DEFAULT 0.00,
+    date_operation DATETIME NOT NULL,
+    FOREIGN KEY (id_emetteur) REFERENCES clients(id),
+    FOREIGN KEY (id_beneficiaire) REFERENCES clients(id),
+    FOREIGN KEY (id_operateur) REFERENCES operateurs(id),
+    FOREIGN KEY (id_type_operation) REFERENCES type_operations(id)
+);
